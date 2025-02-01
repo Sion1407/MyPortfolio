358484a428d84b023754695d235d2b023874e4cc
Sion <sionc140720@gmail.com>
2025-02-02 01:46:52 +0530

    added port

--- model/app.py
+++ model/app.py
@@ -59,4 +59,5 @@ def ask_question(request: QuestionRequest):
 # Run the application
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
